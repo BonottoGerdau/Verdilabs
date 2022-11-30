bf24dc1adfc23bc4644d76434e48ff51b14ad620
Mateus Rafael <99207573+mateusrafael4@users.noreply.github.com>
2022-11-30 18:39:20 -0300

    biblioteca de alertas

---
+++ codigo-fonte/sprint-4/bloco-central/SPRINT_4/alertas.h
@@ -0,0 +1,15 @@
+// Biblioteca local para identificar os erros e promover soluções para cada erro
+
+// Erro 1: Erro de leitura do sensor
+
+
+// Erro 2: Energia / alimentação
+
+
+// Erro 3: Display não funciona
+
+
+// Erro 4: Leds não funcionam
+
+
+// Erro 5: Erro de conexão Wi-Fi
